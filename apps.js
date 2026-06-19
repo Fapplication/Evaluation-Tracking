e4ef47a723c6cca57477e3331aedf098aa65c3b7
Firiiken Belissa GC <41576931+Fapplication@users.noreply.github.com>
2026-06-18 23:52:30 -0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -3,7 +3,7 @@
 //  Include this in every HTML page via <script src="apps.js">
 // ============================================================
 
-const APP_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+const APP_URL = "https://script.google.com/macros/s/AKfycbzpAR52VWNqAlW7N7aqC3StIaTAGmWTE1tDfI0a5YRs6GRB7jqjksYj6HR0eCgwnV0n/exec";
 // ↑ Replace YOUR_DEPLOYMENT_ID with your actual deployment ID after deploying
 
 // ──────────────────────────────────────────────
